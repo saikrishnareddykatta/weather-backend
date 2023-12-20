a4aaaa5c26c9fd8e1e95858d339e3b2d5d65d0bf
Sai Krishna Reddy Katta <saikrishnareddykatta@Sais-MacBook-Pro.local>
2023-12-20 12:08:08 -0800

    feat(login): added login route

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Importing necessary modules
 const express = require("express");
 const axios = require("axios");
-const cors = require("cors"); // Import the 'cors' middleware
+const bcrypt = require("bcrypt");
+const cors = require("cors");
 require("dotenv").config();
+const saltRounds = 10;
 
 // Creating an Express application
 const app = express();
@@ -15,23 +17,44 @@ const users = [
   { id: 3, name: "Bob Johnson", age: 40 },
 ];
 
+async function hashPassword(password) {
+  return await bcrypt.hash(password, saltRounds);
+}
+
+async function authenticateUser(userPassword, hashedPassword) {
+  try {
+    const passwordMatch = await bcrypt.compare(userPassword, hashedPassword);
+    return passwordMatch;
+  } catch (error) {
+    console.error("Password comparison error:", error);
+    return error;
+  }
+}
+
 // Use the cors middleware
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: `${process.env.FRONTEND_URL}`,
+    // origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Define a route to handle GET and POST requests
 app.get("/test-users", (req, res) => {
-  // const { cityName, countryName } = req.query;
-  // console.log("***Query parameters", cityName, countryName);
   res.json(users);
 });
 
 app.post("/register", async (req, res) => {
   const payload = { ...req.body };
-  console.log("***payload", payload);
+  const hashedPassword = await hashPassword(req.body.password);
+  payload.password = hashedPassword;
   try {
     const response = await axios.post(
-      `${process.env.REGISTER_USER_URL}`,
+      `${process.env.CSP_ENDPOINT_URL}`,
       payload,
       {
         headers: {
@@ -39,18 +62,45 @@ app.post("/register", async (req, res) => {
         },
       }
     );
-    console.log("***response", response);
     if (response.status === 200) {
-      const data = response.data.item;
-      res.json(data);
+      const { username } = response.data.item;
+      res.json({ username });
     } else {
       res
         .status(response.status)
         .json({ error: "API Failed due to some issue" });
     }
   } catch (error) {
-    console.log("***error", error);
     res.status(500).json({ error: error, message: error?.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const response = await axios.get(
+      `${process.env.CSP_ENDPOINT_URL}`,
+      { data: { username } },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response.status === 200) {
+      const hashedValue = response.data.user.password;
+      const comparedValue = await authenticateUser(password, hashedValue);
+      if (comparedValue) {
+        res.status(200).json({ message: "Login is successful", username });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    } else {
+      res.status(404).json({ message: "User is not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(users);
   }
 });
 
